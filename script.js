eb911ca368ff3688f93dde313e49e207aded3157
Suraj Khadse <50524919+SurajK23@users.noreply.github.com>
2022-07-15 21:25:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //   "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
 async function myData() {
   const response = await axios.get(
-    "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D"
+    "https://admin-panel-data-edyoda-sourav.herokuapp.com/admin/data"
   );
   console.log(response.data);
   DataAll = response.data;
